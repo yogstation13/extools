93bcef7372d4c3ec866e0dc93cd01bfed90b649b
Valtos <valtos@spaces.ru>
2021-04-23 09:18:10 +0700

    ass blast

--- byond-extools/src/core/byond_structures.cpp
+++ byond-extools/src/core/byond_structures.cpp
@@ -358,3 +358,27 @@ ManagedValue::~ManagedValue()
 {
 	DecRefCount(type, value);
 }
+
+Params MiscEntry::as_params()
+{
+	return Params {
+		parameters2.count(),
+		parameters2.params,
+	};
+}
+
+LocalVars MiscEntry::as_locals()
+{
+	return LocalVars {
+		local_vars2.count,
+		local_vars2.var_name_indices,
+	};
+}
+
+ProcBytecode MiscEntry::as_bytecode()
+{
+	return ProcBytecode {
+		bytecode2.bytecode_length,
+		&bytecode2.bytecode,
+	};
+} 
