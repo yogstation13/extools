9d09c800055fc020997bd39686f56bdc22a19841
Gary Lafortune <mom-puter@hotmail.com>
2021-09-10 18:20:09 -0500

    First attempt at Joao hook-up
    The Joao/main repository is now a Git submodule within /joao_repo, especially prepared to work as this hunk of code compiled along with the rest of the extools DLL.

---
+++ byond-extools/src/joao/joao.cpp
@@ -0,0 +1,146 @@
+#include "joao.h"
+#include "../core/core.h"
+#include <sstream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <forward_list>
+#include <list>
+#include <stack>
+
+namespace // To make absolutely sure no other part of the code tries to link to all this shit
+{
+	namespace joao
+	{
+#define JOAO_NO_INCLUDE_STD // A kludge necessary to avoid namespace problems associated with wrapping all of João in this funky namespace block
+#define JOAO_SAFE //not-as-much-of-a kludge used to enable throttling and disable some dangerous libraries such as the /file Object type
+#include "./joao_repo/AST.cpp"
+#include "./joao_repo/Directory.cpp"
+#include "./joao_repo/Interpreter.cpp"
+
+#include "./joao_repo/nativefuncs/error.cpp"
+#include "./joao_repo/nativefuncs/math.cpp"
+#include "./joao_repo/nativefuncs/string.cpp"
+#include "./joao_repo/nativefuncs/tablelib.cpp"
+
+#include "./joao_repo/Object.cpp"
+#include "./joao_repo/Parser.cpp"
+#include "./joao_repo/Program.cpp"
+#include "./joao_repo/Scanner.cpp"
+#include "./joao_repo/Table.cpp"
+#undef JOAO_NO_INCLUDE_STD
+#undef JOAO_SAFE
+	}
+}
+
+//for the sake of being explicit, since there is a minor name collision going on here
+using ByondValue = Value;
+using JoaoValue = joao::Value;
+
+namespace
+{
+	namespace ID
+	{
+		int data;
+		int content;
+		int message;
+		int frequency;
+		int name;
+	}
+}
+
+
+const char* enable_joao()
+{
+	// get the var IDs for SANIC SPEED
+	ID::data = Core::GetStringId("data", true);
+	ID::content = Core::GetStringId("content", true);
+	ID::message = Core::GetStringId("message", true);
+	ID::frequency = Core::GetStringId("frequency", true);
+	ID::name = Core::GetStringId("name", true);
+
+	//Set up hooks
+	Core::get_proc("/proc/run_script").hook(run_script);
+	return "ok";
+}
+
+trvh run_script(unsigned int argn, ByondValue* args, ByondValue src)
+{
+	using namespace joao;
+	if(argn < 2) // If we weren't given a script or signal
+		return ByondValue::False();
+
+	ByondValue& script = args[0];
+	if(script.type != DataType::STRING) // If what we were given can't be a script
+		return ByondValue::False();
+
+	ByondValue& signal = args[1];
+	if(signal.type != DataType::DATUM) // If what we were given isn't even a datum (signals are, this comment upholding, /datum/signal/subspace/vocal)
+		return ByondValue::False();
+
+	std::istringstream stream = std::istringstream(std::string(script));
+
+	Scanner scanner;
+	scanner.scan(stream);
+	Parser pears(scanner);
+
+	//Tries to follow the structure of the script_signal var from NTSL
+	//The default values are not based on the actual incoming signal, since they are used whenever *any* /signal Object is instantiated within Joao.
+	ObjectType signal_type("signal");
+	JoaoValue empty_string = JoaoValue(std::string(""));
+	signal_type.set_typeproperty_raw("content", empty_string);
+	signal_type.set_typeproperty_raw("freq", 0);
+	signal_type.set_typeproperty_raw("source", empty_string);
+
+
+	pears.IncludeAlienType(&signal_type);
+	Program parsed = pears.parse();
+
+	Interpreter interpreter(parsed, false);
+	
+	//Constructing the João Object to be passed as the argument
+	Object* signal_obj = signal_type.makeObject(interpreter, {});
+	signal_obj->set_property_raw("content", JoaoValue(Core::GetStringFromId(signal.get_by_id(ID::data).get_by_id(ID::message).value)));
+	signal_obj->set_property_raw("freq", JoaoValue(signal.get_by_id(ID::frequency).valuef)); // I think?
+	signal_obj->set_property_raw("source", JoaoValue(Core::GetStringFromId(signal.get_by_id(ID::name).value)));
+
+	JoaoValue jargs = interpreter.makeBaseTable({ JoaoValue(signal_obj) }, {}, nullptr);
+
+	//Execute!
+	JoaoValue ret = interpreter.execute(parsed, jargs);
+	if (ret.t_vType != JoaoValue::vType::Object || ret.t_value.as_object_ptr->object_type != "/signal") // If the program returned something that isn't a signal object
+	{
+		return ByondValue::False();
+	}
+	Object*& retptr = ret.t_value.as_object_ptr; // $#(@$*@!!!!
+
+	//Content typecheck & set
+	JoaoValue& ret_content = retptr->get_property_raw("content");
+	if (ret_content.t_vType != JoaoValue::vType::String)
+	{
+		return ByondValue::False();
+	}
+	signal.get_by_id(ID::data).set_by_id(ID::message, *ret_content.t_value.as_string_ptr);
+
+	//Frequency typecheck & set
+	JoaoValue& ret_freq = retptr->get_property_raw("freq");
+	if (ret_freq.t_vType != JoaoValue::vType::Integer)
+	{
+		return ByondValue::False();
+	}
+	signal.get_by_id(ID::data).set_by_id(ID::frequency, ret_freq.t_value.as_int);
+
+	//Source typecheck & set
+	JoaoValue& ret_source = retptr->get_property_raw("source");
+	if (ret_source.t_vType != JoaoValue::vType::String)
+	{
+		return ByondValue::False();
+	}
+	signal.get_by_id(ID::data).set_by_id(ID::name, *ret_source.t_value.as_string_ptr);
+
+	return ByondValue::True();
+}
+
+
---
+++ byond-extools/src/joao/joao.h
@@ -0,0 +1,5 @@
+#pragma once
+#include "../core/byond_structures.h"
+trvh run_script(unsigned int, Value*, Value); // João also has a Value class, so...
+
+const char* enable_joao();
