1e95b789eca7092f6b0d1312bc6005a60b77ac2e
Jamie D <993128+JamieD1@users.noreply.github.com>
2023-05-10 23:19:20 +0100

    Test

--- byond-extools/src/monstermos/monstermos.cpp
+++ byond-extools/src/monstermos/monstermos.cpp
@@ -503,7 +503,9 @@ const char* enable_monstermos()
 	//Set up gas types map
 	std::vector<Value> nullvector = { Value(0.0f) };
 	Container gas_types_list = Core::get_proc("/proc/gas_types").call(nullvector);
-	Container meta_gas_info = Value::Global().get("meta_gas_info");
+	Value GLOB = Value::Global().get("GLOB");
+	if (!GLOB) return "GLOB is null!";
+	Container meta_gas_info = GLOB.get("meta_gas_info");
 	int gaslen = gas_types_list.length();
 	if (gaslen != TOTAL_NUM_GASES) {
 		return "TOTAL_NUM_GASES does not match the number of /datum/gas subtypes!!";
