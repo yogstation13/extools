ae6e0e9bc9da452eee8fa52f65c09da0d2e6e29c
Jamie D <993128+JamieD1@users.noreply.github.com>
2023-05-11 17:29:50 +0100

    idfk

--- byond-extools/src/monstermos/monstermos.cpp
+++ byond-extools/src/monstermos/monstermos.cpp
@@ -442,7 +442,7 @@ trvh refresh_atmos_grid(unsigned int args_len, Value* args, Value src)
 }
 
 void initialize_gas_overlays() {
-	Container meta_gas_info = Value::Global().get("meta_gas_info");
+	Container meta_gas_info = GetAssocElement(0x52, 0, STRING, Core::GetStringId("meta_gas_info"));
 	if (!meta_gas_info.type) return;
 	for (int i = 0; i < TOTAL_NUM_GASES; ++i)
 	{
@@ -501,7 +501,7 @@ const char* enable_monstermos()
 	//Set up gas types map
 	std::vector<Value> nullvector = { Value(0.0f) };
 	Container gas_types_list = Core::get_proc("/proc/gas_types").call(nullvector);
-	Container meta_gas_info = Value::Global().get("meta_gas_info");
+	Container meta_gas_info = GetAssocElement(0x52, 0, STRING, Core::GetStringId("meta_gas_info"));
 	int gaslen = gas_types_list.length();
 	if (gaslen != TOTAL_NUM_GASES) {
 		return "TOTAL_NUM_GASES does not match the number of /datum/gas subtypes!!";
